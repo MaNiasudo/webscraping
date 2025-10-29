858426c46ad08ffe6e869285c03cd232551e0e35
TahaEfazati <Wheremania@gmail.com>
2025-10-29 10:28:00 +0330

    added price with tag and pair them together

--- scraperp.py
+++ scraperp.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 
-with open ("index.html", "r") as html_file :
+with open ("index.html", "r") as html_file : # Request where  you want to scrape
     content = html_file.read()
 
-    soup = BeautifulSoup(content, "lxml")
-    tags = soup.find_all('h5')
-    print(tags)
-    
+    soup = BeautifulSoup(content, "lxml") # Make a soup curser i guess?
+    courses_html_tags = soup.find_all('h5') #Search for tag and use find all to find all the tags not only first tag
+    prices = soup.find_all('a') 
+    for price,course in zip(prices,courses_html_tags): #zip them so we dont face valueerror
+        print(price.text,course.text)
